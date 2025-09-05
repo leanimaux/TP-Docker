e5a5532ed5890a58c1926c82ea538dfda5da28ce
leanimaux <149502238+leanimaux@users.noreply.github.com>
2025-09-05 08:54:52 +0200

    feat: exo 6

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
+# Connexion à Mongo (nom du service docker-compose = mongo)
+client = MongoClient("mongodb://mongo:27017/")
+db = client["ma_base"]
+collection = db["messages"]
+
 @app.route("/")
 def hello():
+    collection.insert_one({"msg": "Hello depuis Flask & Mongo 🚀"})
     return "Connexion réussie à MongoDB !"
 
 if __name__ == "__main__":
